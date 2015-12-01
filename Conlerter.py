346d4e9f0303fc3340e8cb35d13101f0ea971fb4
Metal.H47K <metal.h47k@hotmail.com>
2015-12-01 23:18:36 +0000

    Created Looping Functions! I just need to code in the lines that switches between the loops

--- Conlerter.py
+++ Conlerter.py
@@ -1,4 +1,6 @@
-#print("Sub Routine Application") Monitors the network connection and plays a sound if the connection goes down
+﻿# ("Sub Routine Application") Monitors the network connection and plays a sound if the connection goes down
+
+#### WORK IN PROGRESS ####
 
 #    Conlerter version 1, Copyright (C) 2015 Stephen Jack Henry
 #    Conlerter comes with ABSOLUTELY NO WARRANTY.
@@ -18,21 +20,51 @@ def NetSucc():
 
 ips=[]
 n = 1
+NetSuccess = 10
+NetFailure = 10
+PinSuc = 0
+PinFail = 0
 x = '8.8.8.8'
 ips.append(x)
 for ping in range(0,n):
     ipd=ips[ping]
-res = subprocess.call(['ping', '-n', '1', ipd]) # I need to code this so it does a continuous ping and when it goes out for more then 20 seconds it plays a soun0d
 
-# http://www.python-course.eu/python3_loops.php
-# https://www.google.co.uk/search?q=if+statement+when+loop+has+executed+a+number+of+times&oq=if+statement+when+loop+has+executed+a+number+of+times&aqs=chrome..69i57.31087j0j4&sourceid=chrome&es_sm=122&ie=UTF-8#q=if+statement+while+loop+has+executed+a+number+of+times+python
+def PingFailure():
+    while PinFail < NetSuccess:
+        res = subprocess.call(['ping', '-n', '10', ipd])
+    if ipd in str(res):
+        PingSuccess()
+    else:
+        print ("ping to", ipd, "failed!"), NetFail()
 
-#res = subprocess.call(['ping', '-n', '4', ipd])
+def PingSuccess():
+    while PinFail < NetFailure: # This needs to be cleaned up so it doesn't interfere with the other function
+        res = subprocess.call(['ping', '-n', '10', ipd])
+    if ipd in str(res):
+        PingFail()
+    else:
+        print ("ping to", ipd, "successful!"), NetSucc()
+
+# I've defined the main looping fuctions. 
+
+print('Starting Monitor'); PingSuccess()
+
+# So it runs the main loop until the connection fails: It plays a sound and runs the second loop until the connection is
+# restablished, plays a sound and it switches back to the other loop. I only want it to play the sound when switching 
+# to the other loop
+
+
+
+    #except "failure" in str(res):
+    #    print('Conection Stable', NetSucc())
+    #    PinSuc += 1
+
 #if ipd in str(res):
 #    print ("ping to", ipd, "OK") # I need to code it so if it's 10 seconds of stable pings again it will play a connection sound
 #elif "failure" in str(res):
 #    print ("ping to", ipd, "recieved no responce", NetFail())
 #else:
 #    print ("ping to", ipd, "failed!", NetFail())
+#except "failure" in str(res):
 
 
