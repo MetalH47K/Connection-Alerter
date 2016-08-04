25e3d4ccc73a96a654743cb4202b9f6af2b8007c
Metal.H47K <MetalH47K@users.noreply.github.com>
2016-08-04 13:51:32 +0100

    Update Conlerter.py

--- Conlerter.py
+++ Conlerter.py
@@ -7,9 +7,6 @@
 #    This is free software, and you are welcome to redistribute it
 #    under certain conditions; type `show c' for details.
 
-
-
-
 # NOTES
 
 # I have a basic understanding of CLASS and I am still trying to wrap my head around __int__ and self declarations. In the context
@@ -17,48 +14,78 @@
 # I have made a small CLASS at the top to practice turning the two sound functions I had into a CLASS. It doesn't seem I need to 
 # but it's about learning it more so than practibility at the moment. 
 
+import os
+import subprocess
+import winsound
+import time
 
-# TEST & OLD CODE BELOW
-
-# Sound.NetFail()
-# Sound.NetSucc()
-
-#def PingFailure():
-#    while PinFail < NetSuccess:
-#        res = subprocess.call(['ping', '-n', '10', ipd])
-#    if ipd in str(res):
-#        PingSuccess()
-#    else:
-#        print ("ping to", ipd, "failed!"), NetFail()
-
-#def PingSuccess():
-#    while PinFail < NetFailure: # This needs to be cleaned up so it doesn't interfere with the other function
-#        res = subprocess.call(['ping', '-n', '10', ipd])
-#    if ipd in str(res):
-#        PingFail()
-#    else:
-#        print ("ping to", ipd, "successful!"), NetSucc()
-
-## I've defined the main looping fuctions. 
-
-#print('Starting Monitor'); PingSuccess()
-
-# So it runs the main loop until the connection fails: It plays a sound and runs the second loop until the connection is
-# restablished, plays a sound and it switches back to the other loop. I only want it to play the sound when switching 
-# to the other loop
+class ConSound():
+    def NetFail():
+        winsound.Beep(2000 , 180), winsound.Beep(1400 , 180)
+    def NetSucc():
+        winsound.Beep(1400 , 250), winsound.Beep(2000 , 250), winsound.Beep(3600 , 280)
 
 
+#IPS = [] #Create List
+#x = '8.8.8.8' #Add IP Address
+#ips.append(x) #Add x to list
+#for ping in range(0,1): 
+#    ipd = ips[ping]
 
-    #except "failure" in str(res):
-    #    print('Conection Stable', NetSucc())
-    #    PinSuc += 1
+IPAddress = "8.8.8.8"
 
-#if ipd in str(res):
-#    print ("ping to", ipd, "OK") # I need to code it so if it's 10 seconds of stable pings again it will play a connection sound
-#elif "failure" in str(res):
-#    print ("ping to", ipd, "recieved no responce", NetFail())
-#else:
-#    print ("ping to", ipd, "failed!", NetFail())
-#except "failure" in str(res):
+class IP(object):
+	#Below are the varibles
+	StateChangeNumber = 15            # number of success/failure pings to record new state
+	PingReply = "Reply from"
+	FailedPing = "Request timed out"   # word indicating unreachable host 
+	UnableToFindHost = "Ping request could not find host"
+	UnreachablePing = "Destination Host Unreachable"
+	###
+	#Below is the code efficient abreviations of the above. 
+	###
+    PingR = PingReply
+	PingF = FailedPing
+	PingH = UnableToFindHost
+	PingU = UnreachablePing
+	SCN = StateChangeNumber            # number of success/failure pings to record new state
+    def __init__(self, ip, failfunc, succfunc, initial = True):
+        self.ip = ip
+        self.failfunc = failfunc  # to warn of a disconnection
+        self.succfunc = succfunc  # to warn of a connection
+        self.connected = initial  # start by default in connected state
+        self.curr = 0             # number of successive alternate states
+    def PingTest(self):
+        p = subprocess.Popen(['ping', '-n', '1', self],
+                     stdout = subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if PingF in stdout: #Command in question This needs to be reviewed. 
+            if self.connected:
+                self.curr += 1
+            else:
+                self.curr = 0   # reset count
+        else:
+            if not self.connected:
+                self.curr += 1
+            else:
+                self.curr = 0   # reset count
+        if self.curr >= self.SCN:     # state has changed
+            self.connected = not self.connected
+            self.curr = 0
+            if self.connected:        # warn for new state
+                self.succfunc(self)
+            else:
+                self.failfunc(self)
+	def EmailState(): #TEST CLASS FUNCTION
+		print("Email Addres not found in database")
+		
 
+### Need to create two states... On and Off (True and False)
+### Then do a ping test and if its up for 15 pings or down for 15 pings change the state from the other
+### And then play a sound. 
+		
+		
+		
+# MAIN PROGRAM
 
+IP.PingTest(IPAddress)
